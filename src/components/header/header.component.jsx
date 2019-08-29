85a9a9fa0a3f3532a23121d895b794c9922dccb1
Ekow244 <daniel.ekow.baidoo@gmail.com>
2019-08-29 00:09:36 +0000

    Added toggling of cart icon and implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,10 +6,11 @@ import './header.styles.scss';
 import {auth} from '../../firebase/firebase.util';
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import{ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header=({currentUser})=>(
+const Header=({currentUser,hidden})=>(
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -36,11 +37,17 @@ const Header=({currentUser})=>(
             }
             <CartIcon/>
         </div>
+        {
+            hidden ? null : 
+           <CartDropdown/>  
+        }
+        
     </div>
 );
 
-const mapStateToProps=state =>({
-    currentUser:state.user.currentUser
+const mapStateToProps=({user:{currentUser},cart:{hidden}}) =>({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
